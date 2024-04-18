4f8ad1eee530b3910132e80c8258beadbe9ac581
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-04-18 10:08:50 +0800

    add express-handlebras& build basic homepage

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const { pages, apis } = require('./routes')
+const handlebars = require('express-handlebars')
 
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
+/*************************************************************** */
 app.use(express.urlencoded({ extended: true }))
 app.use('/api', apis)
 app.use(pages)
+/***************************************************************** */
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
 })
--- routes/pages/index.js
+++ routes/pages/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  return res.send('this is from routes/pages.js')
+  return res.render('home')
 })
 
 module.exports = router
