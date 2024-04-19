a8c77117b09ad1a474fcb0698da5bca47fc54f96
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-04-19 17:40:01 +0800

    add apply page & building and mapping course table

---
+++ migrations/20240419093608-create-course.js
@@ -0,0 +1,49 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      introduction: {
+        type: Sequelize.STRING
+      },
+      style: {
+        type: Sequelize.STRING
+      },
+      day_of_week: {
+        allowNull: false,
+        type: Sequelize.JSON
+      },
+      duration: {
+        type: Sequelize.STRING
+      },
+      class_link: {
+        type: Sequelize.STRING
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Courses');
+  }
+};
