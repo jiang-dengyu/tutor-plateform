ebbfe6f1447f59baed89bccbf75091d241aa22e1
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-04-24 21:30:23 +0800

    add comment model

---
+++ models/comment.js
@@ -0,0 +1,29 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  comment.init(
+    {
+      userId: DataTypes.INTEGER,
+      courseId: DataTypes.INTEGER,
+      score: DataTypes.INTEGER,
+      comment: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'Comment',
+      tableName: 'Comments',
+      underscored: true
+    }
+  )
+  return comment
+}
