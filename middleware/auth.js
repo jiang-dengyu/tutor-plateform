73b2ceee12deb60cbeba4f0e167ad632927780f2
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-04-30 10:45:08 +0800

    add flash message when authenticated a user or admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,9 +6,11 @@ const authenticatedUser = (req, res, next) => {
     if (!helpers.getUser(req).isAdmin) {
       return next()
     } else {
+      req.flash('error_messages', '管理者只能訪問候台')
       return res.redirect('/admin/users')
     }
   } else {
+    req.flash('error_messages', '尚未登入')
     return res.redirect('/signin')
   }
 }
@@ -17,8 +19,10 @@ const authenticatedAdmin = (req, res, next) => {
   // if (req.isAuthenticated)
   if (helpers.ensureAuthenticated(req)) {
     if (helpers.getUser(req).isAdmin) return next()
+    req.flash('error_messages', '非管理者不能訪問後台')
     res.redirect('/home')
   } else {
+    req.flash('error_messages', '尚未登入')
     res.redirect('/signin')
   }
 }
