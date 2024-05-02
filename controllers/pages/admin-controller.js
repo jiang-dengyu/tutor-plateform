506652c869539458e981e802af31441c3c6d3538
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-05-02 09:52:07 +0800

    add users inform in admin user page

--- controllers/pages/admin-controller.js
+++ controllers/pages/admin-controller.js
@@ -1,7 +1,13 @@
+const { User } = require('../../models')
+
 /******************************* */
 const adminController = {
-  adminUsers: (req, res) => {
-    return res.render('admin/user')
+  adminUsers: (req, res, next) => {
+    User.findAll({ raw: true })
+      .then((users) => {
+        return res.render('admin/user', { users })
+      })
+      .catch((err) => next(err))
   }
 }
 /******************************* */
