605a59102028200418f39850785d8824e74ad9dc
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-05-03 16:00:04 +0800

    add apiErrorHandler

--- controllers/apis/course-controller.js
+++ controllers/apis/course-controller.js
@@ -2,7 +2,7 @@ const courseServices = require('../../services/course-services')
 /******************************* */
 const courseController = {
   home: (req, res, next) => {
-    courseServices.home(req, (err, data) => (err ? next(err) : res.json(data)))
+    courseServices.home(req, (err, data) => (err ? next(err) : res.json({ status: 'success', data })))
   }
 }
 /******************************* */
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const courseController = require('../../controllers/apis/lesson-controller')
-
-const { authenticatedUser, authenticatedAdmin } = require('../../middleware/auth.js')
-const passport = require('../../config/passport')
-
-const upload = require('../../middleware/multer')
-const { generalErrorHandler } = require('../../middleware/error-handler')
+const courseController = require('../../controllers/apis/course-controller.js')
+const { apiErrorHandler } = require('../../middleware/error-handler')
 
 /*********************************** */
 
 router.get('/home', courseController.home)
+router.use('/', apiErrorHandler)
 /******************************************************** */
 module.exports = router
