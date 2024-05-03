7b4f28aa137986fd7ed07fd299a73d3e096f0fb6
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-05-03 12:20:14 +0800

    add home-services

---
+++ services/course-services.js
@@ -0,0 +1,34 @@
+const { User, Course } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helpers.js')
+/****************************** */
+const courseServices = {
+  home: (req, cb) => {
+    const DEFAULT_LIMIT = 6
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(page, limit)
+    Promise.all([
+      Course.findAndCountAll({
+        limit,
+        offset,
+        raw: true
+      }),
+      User.findAll({
+        attributes: ['name', 'totalHours'],
+        order: [['totalHours', 'DESC']],
+        limit: 5,
+        raw: true
+      })
+    ])
+      .then(([courses, allUsers]) => {
+        return cb(null, {
+          courses: courses.rows,
+          pagination: getPagination(page, limit, courses.count),
+          allUsers
+        })
+      })
+      .catch((err) => cb(err))
+  }
+}
+/******************************************************** */
+module.exports = courseServices
