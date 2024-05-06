b28a91b0f1690435a44cc28c943ec9ce8bd7e419
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-05-06 14:49:05 +0800

    api-auth middleware optimization

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,14 +1,20 @@
 const passport = require('../config/passport')
 
 /*************************** */
-const apiAuthenticated = passport.authenticate('jwt', { session: false })
+const apiAuthenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+    req.user = user //passport.authenticate自行寫cb函式的話，req.user要自己放進去
+    next()
+  })(req, res, next)
+}
 const apiAuthenticatedAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) return next()
   return res.status(403).json({ status: 'error', message: '非後台管理者帳號無法進入後台' })
 }
 const apiAuthenticatedUser = (req, res, next) => {
-  if (req.user && !req.user.isAdmin) return next()
-  return res.status(403).json({ status: 'error', message: '後台管理者帳號無法進入前台' })
+  if (req.user && req.user.isAdmin) return res.status(403).json({ status: 'error', message: '後台管理者帳號無法進入前台' })
+  return next()
 }
 module.exports = {
   apiAuthenticated,
